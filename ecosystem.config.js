9f448b70a8115ccf12ce067ceabdf3c4004c3de3
KrishCodesw <krishjain.w@gmail.com>
2026-05-07 15:47:48 +0530

    added ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "dashboard",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+        PORT: 4003,
+      },
+    },
+  ],
+};
